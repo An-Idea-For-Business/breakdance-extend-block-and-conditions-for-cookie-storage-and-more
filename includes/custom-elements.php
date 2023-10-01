4614271274ecedb9a9a2f1c67064f9413bf8e405
Edoardo guzzi <mredodos@gmail.com>
2023-10-01 12:11:42 +0200

    active elements studio

--- includes/custom-elements.php
+++ includes/custom-elements.php
@@ -1,3 +1,34 @@
 <?php
 
 namespace AIFB\BreakdanceCustomElements;
+
+use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
+
+add_action('breakdance_loaded', function () {
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/elements',
+        'BreakdanceCustomElements',
+        'element',
+        'Custom Elements',
+        false
+    );
+
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/macros',
+        'BreakdanceCustomElements',
+        'macro',
+        'Custom Macros',
+        false,
+    );
+
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/presets',
+        'BreakdanceCustomElements',
+        'preset',
+        'Custom Presets',
+        false,
+    );
+},
+    // register elements before loading them
+    9
+);
